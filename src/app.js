6fb56f32a55c194e532893f643bf3a600018162d
glyphack <sh.hooshyari@gmail.com>
2020-07-24 21:53:18 +0430

    use const instead of var for imports :poop:

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require("mongoose");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
