ca7e1e6b30beab96dac8640b65a6088489accb82
glyphack <sh.hooshyari@gmail.com>
2020-07-24 23:41:53 +0430

    use winston for logging :wrench:

--- src/app.js
+++ src/app.js
@@ -4,23 +4,27 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const morgan = require('morgan');
 const mongoose = require('mongoose');
+const winston = require('./config/winston');
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Connected to MongoDB 🔥'));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => winston.info('Connected to MongoDB 🔥'));
 
 const app = express();
+
+app.use(morgan('combined', { stream: winston.stream }));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 require('./config/steam')(app);
 
-app.use(logger('dev'));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -38,6 +42,12 @@ app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  winston.error(
+    `${err.status || 500} - ${err.message} - ${req.originalUrl} - ${
+      req.method
+    } - ${req.ip}`,
+  );
 
   // render the error page
   res.status(err.status || 500);
