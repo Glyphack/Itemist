790235ea788c94c9dacd011851f6bb1c56a9a02b
glyphack <sh.hooshyari@gmail.com>
2020-07-29 23:27:13 +0430

    add user routes

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const winston = require('./config/winston');
+const jwtMiddleWare = require('./middlewares/auth.js');
+
+const authRoutes = require('./routes/auth.routes');
+const userRoutes = require('./routes/users.routes');
 
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -24,13 +28,15 @@ app.set('views', path.join(__dirname, '/views'));
 
 require('./config/steam')(app);
 
-app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/auth', require('./routes/auth.routes'));
+// app.use(jwtMiddleWare);
+
+app.use('/auth', authRoutes);
+app.use('/user', jwtMiddleWare, userRoutes);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
