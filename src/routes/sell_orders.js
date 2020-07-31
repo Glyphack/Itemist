290c5f9249f64cdd88b7956500c9e8a8020cd3f1
glyphack <sh.hooshyari@gmail.com>
2020-07-31 16:52:44 +0430

    implement sell order routes

---
+++ src/routes/sell_orders.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const SellOrder = require('../models/sell_order');
+const User = require('../models/user.model');
+const { manager } = require('../utils/trade_offer_manager');
+const winston = require('../config/winston');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const user = await User.findOne({ steamId: req.user.steamId });
+  const sellOrders = await SellOrder.find({ seller: user });
+  res.send(sellOrders);
+});
+
+router.post('/', async (req, res) => {
+  const appId = req.body.app_id;
+  const contextId = req.body.context_id;
+  const assetId = req.body.asset_id;
+  const { price } = req.body;
+
+  manager.getUserInventoryContents(
+    req.user.steamId,
+    appId,
+    contextId,
+    true,
+    async (err, inventory) => {
+      if (err) {
+        winston.log(err);
+      } else {
+        const item = inventory.find((i) => i.assetid === assetId);
+        const user = await User.findOne({ steamId: req.user.steamId });
+        const sellOrder = await SellOrder.create({
+          seller: user,
+          price,
+          appId: item.appid,
+          contextId: item.contextid,
+          assetId: item.assetid,
+        });
+        res.json(sellOrder);
+      }
+    },
+  );
+});
+module.exports = router;
