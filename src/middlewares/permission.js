22c6ce84c2076b12166752bb6ac99920bd9c47a0
glyphack <sh.hooshyari@gmail.com>
2020-08-05 22:26:50 +0430

    remove debug print

--- src/middlewares/permission.js
+++ src/middlewares/permission.js
@@ -2,7 +2,6 @@ function isOwner(req, res, next) {
   if (req.user.steamId === req.params.steamId) {
     next();
   } else {
-    console.log(req.user.steamId, req.params);
     res.status(403);
     res.send("You don't have permission to access this information");
   }
