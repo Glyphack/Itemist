3ccb47aa656c1684ad0080e86df8eec9db3debac
glyphack <sh.hooshyari@gmail.com>
2020-08-06 18:46:31 +0430

    add hook to create product for succesful sell orders

--- src/models/sellOrder.model.js
+++ src/models/sellOrder.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const createProductFromSellOrder = require('../api/products/products.services');
 
 const sellOrderSchema = new mongoose.Schema({
   seller: {
@@ -19,7 +20,15 @@ const sellOrderSchema = new mongoose.Schema({
   },
   tradeStatus: {
     type: String,
+    enum: ['Not sent', 'Pending', 'Succesful', 'Failed'],
+    default: 'Not sent',
   },
+});
+
+sellOrderSchema.post('save', async (doc) => {
+  if (doc.tradeStatus === 'Succesful') {
+    createProductFromSellOrder(doc);
+  }
 });
 
 module.exports = mongoose.model('sellEntry', sellOrderSchema);
