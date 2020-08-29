c48b6fc3f9d0bf21c467c1a39f168acde7c3c9a0
glyphack <sh.hooshyari@gmail.com>
2020-08-29 23:57:30 +0430

    refactor code

--- src/app.js
+++ src/app.js
@@ -9,9 +9,12 @@ const mongoose = require('mongoose');
 const Sentry = require('@sentry/node');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
+const bodyParser = require('body-parser');
 
 const {logger} = require('./config/winston');
 const routesV1 = require('./api/routes');
+const manager = require('./utils/bot');
+
 
 const app = express();
 
@@ -26,6 +29,7 @@ mongoose.connect(process.env.MONGO_URI, {
     throw new Error('Could not connect to database' + e);
   });
 
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
@@ -34,16 +38,20 @@ const swaggerDocument = YAML.load('./docs/OpenAPI/itemist.yaml');
 
 app.use(Sentry.Handlers.requestHandler());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(morgan('combined', { stream: logger.stream }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/v1', routesV1);
 
 app.use(Sentry.Handlers.errorHandler());
 
-// catch 404 a)nd forward to error handler
+// catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
