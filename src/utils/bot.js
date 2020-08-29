00818c4c0eadd61548538bae108fe0f36f5e8aee
glyphack <sh.hooshyari@gmail.com>
2020-08-30 01:31:59 +0430

    add new bot

--- src/utils/bot.js
+++ src/utils/bot.js
@@ -1,95 +1,122 @@
+const util = require('util')
+
 const TradeOfferManager = require('steam-tradeoffer-manager');
 const SteamCommunity = require('steamcommunity');
 const SteamTotp = require('steam-totp');
 const SteamUser = require('steam-user');
 
-const client = require('./steam_client');
+const client = new SteamUser();
+const community = new SteamCommunity();
+const manager = new TradeOfferManager({
+  steam: client,
+  community: community,
+  language: 'en',
+  'domain': 'itemist.ir',
+});
 
-class SteamBot {
-  constructor(logOnOptions) {
-    this.client = client;
-    this.community = new SteamCommunity();
-    this.manager = new TradeOfferManager({
-      steam: this.client,
-      community: this.community,
-      language: 'en'
-    });
-
-    this.logOn(logOnOptions);
-  }
-
-  logOn(logOnOptions) {
-    this.client.logOn(logOnOptions);
-
-    this.client.on('loggedOn', () => {
-      console.log('Logged into Steam');
-
-      this.client.setPersona(SteamUser.EPersonaState.Online);
-      this.client.gamesPlayed(730);
-    });
-
-    this.client.on('webSession', (sessionid, cookies) => {
-      this.manager.setCookies(cookies);
-
-      this.community.setCookies(cookies);
-      this.community.startConfirmationChecker(10000, process.env.STEAM_ACCOUNT_IDENTITY_SECRET);
-    });
-  }
-
-  sendDepositTrade(partner, assetid, callback) {
-    const offer = this.manager.createOffer(partner);
-
-    this.manager.getUserInventoryContents(partner, 730, 2, true, (err, inv) => {
-      if (err) {
-        console.log(err);
-      } else {
-        const item = inv.find((item) => item.assetid == assetid);
-
-        if (item) {
-          offer.addTheirItem(item);
-          offer.setMessage('Deposit item on the website!');
-          offer.send((err, status) => {
-            callback(err, (status === 'sent' || status === 'pending'), offer.id);
-          });
-        } else {
-          callback(new Error('Could not find item'), false);
-        }
-      }
-    });
-  }
-
-  sendWithdrawTrade(partner, credits, assetid, callback) {
-    const offer = this.manager.createOffer(partner);
-
-    this.manager.getInventoryContents(730, 2, true, (err, inv) => {
-      if (err) {
-        console.log(err);
-      } else {
-        const item = inv.find((item) => item.assetid === assetid);
-
-        if (item) {
-
-          // Check to make sure the user can afford the item here
-
-          offer.addMyItem(item);
-          offer.setMessage('Withdraw item from the website!');
-          offer.send((err, status) => {
-            callback(err, (status === 'sent' || status === 'pending'), offer.id);
-          });
-        } else {
-          callback(new Error('Could not find item'), false);
-        }
-      }
-    });
-  }
-}
-
-const bot = new SteamBot({
-  accountName: process.env.STEAM_ACOUNT_NAME,
-  password: process.env.STEAM_ACOUNT_PASSWORD,
+const logInOptions = {
+  accountName: process.env.STEAM_ACCOUNT_NAME,
+  password: process.env.STEAM_ACCOUNT_PASSWORD,
   twoFactorCode: SteamTotp.generateAuthCode(
     process.env.STEAM_ACCOUNT_SHARED_SECRET,
   ),
+};
+
+client.logOn(logInOptions);
+
+client.on('loggedOn', () => {
+  console.log('logged on');
+
+  client.setPersona(SteamUser.EPersonaState.Online);
+  client.gamesPlayed(359550);
 });
 
-module.exports = {steamBot: bot}
+client.on('webSession', (sid, cookies) => {
+  manager.setCookies(cookies);
+  community.setCookies(cookies);
+  community.startConfirmationChecker(20000, process.env.STEAM_IDENTITY_SECRET);
+});
+
+community.on("sessionExpired", function (err) {
+  console.error(`not logged in ${err}`);
+  client.webLogOn();
+});
+
+manager.on('newOffer', function (offer) {
+  console.log("New offer #" + offer.id + " from " + offer.partner.getSteam3RenderedID());
+  offer.accept(function (err, status) {
+    if (err) {
+      console.log("Unable to accept offer: " + err.message);
+    } else {
+      console.log("Offer accepted: " + status);
+      if (status == "pending") {
+        community.acceptConfirmationForObject(process.env.STEAM_ACCOUNT_SHARED_SECRET, offer.id, function (err) {
+          if (err) {
+            console.log("Can't confirm trade offer: " + err.message);
+          } else {
+            console.log("Trade offer " + offer.id + " confirmed");
+          }
+        });
+      }
+    }
+  });
+});
+
+async function getUserInventory(userSteamId, appId, contextId, tradableOnly) {
+  const getUserInventoryContentsPromise = util.promisify(manager.getUserInventoryContents.bind(manager));
+  return await getUserInventoryContentsPromise(userSteamId, appId, contextId, tradableOnly);
+}
+
+async function getBotInventory(appId, contextId, tradableOnly) {
+  const getInventoryContentsPromise = util.promisify(manager.getInventoryContents.bind(manager));
+  return await getInventoryContentsPromise(appId, contextId, tradableOnly);
+}
+
+function sendDepositTrade(partner, assetid, callback) {
+  const offer = manager.createOffer(partner);
+
+  manager.getUserInventoryContents(partner, 570, 2, true, (err, inv) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const item = inv.find((item) => item.assetid === assetid);
+
+      if (item) {
+        offer.addTheirItem(item);
+        offer.setMessage('Deposit item on the website!');
+        offer.send((err, status) => {
+          callback(err, (status === 'sent' || status === 'pending'), offer.id);
+        });
+      } else {
+        callback(new Error('Could not find item'), false);
+      }
+    }
+  });
+}
+
+function sendWithdrawTrade(partner, credits, assetid, callback) {
+  const offer = this.manager.createOffer(partner);
+
+  this.manager.getInventoryContents(570, 2, true, (err, inv) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const item = inv.find((item) => item.assetid === assetid);
+
+      if (item) {
+
+        // Check to make sure the user can afford the item here
+
+        offer.addMyItem(item);
+        offer.setMessage('Withdraw item from the website!');
+        offer.send((err, status) => {
+          callback(err, (status === 'sent' || status === 'pending'), offer.id);
+        });
+      } else {
+        callback(new Error('Could not find item'), false);
+      }
+    }
+  });
+}
+
+module.exports = {getUserInventory, getBotInventory, sendDepositTrade, sendWithdrawTrade, manager};
