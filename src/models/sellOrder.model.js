f077e88377b71b4e91c0f15022a16fcf4bed7acc
glyphack <sh.hooshyari@gmail.com>
2020-09-15 21:10:23 +0430

    add timestamps to models

--- src/models/sellOrder.model.js
+++ src/models/sellOrder.model.js
@@ -2,32 +2,34 @@ const mongoose = require('mongoose');
 const {createProductFromSellOrder} = require("../api/products/products.services");
 
 const sellOrderSchema = new mongoose.Schema({
-  seller: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+    seller: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    price: {
+      type: Number,
+    },
+    appId: {
+      type: String,
+    },
+    contextId: {
+      type: String,
+    },
+    assetId: {
+      type: String,
+    },
+    tradeOffer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'TradeOffer',
+    },
+    success: {
+      type: Boolean,
+      default: false,
+    }
   },
-  price: {
-    type: Number,
-  },
-  appId: {
-    type: String,
-  },
-  contextId: {
-    type: String,
-  },
-  assetId: {
-    type: String,
-  },
-  tradeOffer: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'TradeOffer',
-  },
-  success: {
-    type: Boolean,
-    default: false,
-  }
-});
+  {timestamps: true}
+);
 
-sellOrderSchema.index({ appId:1, contextId: 1, assetId: 1 }, { unique: true });
+sellOrderSchema.index({appId: 1, contextId: 1, assetId: 1}, {unique: true});
 
 module.exports = {SellOrder: mongoose.model('SellOrder', sellOrderSchema)};
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,28 +1,30 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-  },
-  steamId: {
-    type: String,
-  },
-  avatar: {
-    type: String,
-  },
-  profileUrl: {
-    type: String,
-  },
-  tradeUrl: {
-    type: String,
-    default: '',
-  },
-  wallet: {
-    balance: {
-      type: Number,
-      default: 0,
+    name: {
+      type: String,
+    },
+    steamId: {
+      type: String,
+    },
+    avatar: {
+      type: String,
+    },
+    profileUrl: {
+      type: String,
+    },
+    tradeUrl: {
+      type: String,
+      default: '',
+    },
+    wallet: {
+      balance: {
+        type: Number,
+        default: 0,
+      },
     },
   },
-});
+  {timestamps: true}
+);
 
 module.exports = {User: mongoose.model('User', userSchema)};
