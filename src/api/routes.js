7671df9c71e461482a1a836fe904b0cea33149c9
Glyphack <sh.hooshyari@gmail.com>
2020-09-29 11:33:22 +0330

    rename users route to profile (#18)

--- src/api/routes.js
+++ src/api/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const jwtMiddleWare = require('../middlewares/auth.js');
 const authRoutes = require('./auth/auth.routes');
-const userRoutes = require('./users/users.routes');
+const profileRoutes = require('./profile/profile.routes');
 const sellOrdersRoutes = require('./sell/sell.routes');
 const productRoutes = require('./products/products');
 const {paymentRoutes} = require("./payment/payment.routes");
@@ -11,7 +11,7 @@ const {cartRoutes} = require("./cart/cart.routes");
 const router = express.Router();
 
 router.use('/auth', authRoutes);
-router.use('/users', jwtMiddleWare, userRoutes);
+router.use('/profile', jwtMiddleWare, profileRoutes);
 router.use('/sell', jwtMiddleWare, sellOrdersRoutes);
 router.use('/products', productRoutes);
 router.use('/cart', cartRoutes);
