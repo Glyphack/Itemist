c20ac5a02005d06d928a04a98e0934d69a521544
glyphack <sh.hooshyari@gmail.com>
2020-10-11 22:04:58 +0330

    Uncomment sell route

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import jwtMiddleWare from '../middlewares/auth';
 import authRoutes from './auth/auth.routes';
 import profileRoutes from './profile/profile.routes';
-// import sellOrdersRoutes from './sell/sell.routes';
+import sellOrdersRoutes from './sell/sell.routes';
 // import productRoutes from './products/products';
 // import paymentRoutes from './payment/payment.routes';
 // import cartRoutes from './cart/cart.routes';
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/profile', jwtMiddleWare, profileRoutes);
-// router.use('/sell', jwtMiddleWare, sellOrdersRoutes);
+router.use('/sell', jwtMiddleWare, sellOrdersRoutes);
 // router.use('/products', productRoutes);
 // router.use('/cart', cartRoutes);
 // router.use('/payment', paymentRoutes);
