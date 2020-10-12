2829f6fdffcdfd0feabcbdb31d85d027b80483a0
glyphack <sh.hooshyari@gmail.com>
2020-10-12 14:33:53 +0330

    Add typecheking to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,13 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
+    project: './tsconfig.json',
   },
   plugins: ['prettier'],
   rules: {
