bf228fefdf8dba2ee5ddb34db17854b48a7c11e9
glyphack <sh.hooshyari@gmail.com>
2020-10-12 19:13:42 +0330

    Remove seller and __v fields from products response

--- src/api/products/products.controllers.ts
+++ src/api/products/products.controllers.ts
@@ -2,8 +2,8 @@ import { Request, Response } from 'express';
 import Product from '../../models/product.model';
 
 async function getProducts(req: Request, res: Response): Promise<void> {
-  const products = await Product.find({});
-  res.send(products);
+  const products = await Product.find({}).select('-__v -seller');
+  res.json(products);
 }
 
 export { getProducts };
