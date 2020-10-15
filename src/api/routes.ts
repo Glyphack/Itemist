ab050987f9a096f23e39be5eaa00bd0d01339c3b
glyphack <sh.hooshyari@gmail.com>
2020-10-15 18:42:12 +0330

    Uncomment payment routes

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -5,7 +5,7 @@ import authRoutes from './auth/auth.routes';
 import profileRoutes from './profile/profile.routes';
 import sellOrdersRoutes from './sell/sell.routes';
 import productRoutes from './products/products.routes';
-// import paymentRoutes from './payment/payment.routes';
+import paymentRoutes from './payment/payment.routes';
 import cartRoutes from './cart/cart.routes';
 
 const router = express.Router();
@@ -15,6 +15,6 @@ router.use('/profile', jwtMiddleWare, profileRoutes);
 router.use('/sell', jwtMiddleWare, sellOrdersRoutes);
 router.use('/products', productRoutes);
 router.use('/cart', cartRoutes);
-// router.use('/payment', paymentRoutes);
+router.use('/payment', paymentRoutes);
 
 export = router;
