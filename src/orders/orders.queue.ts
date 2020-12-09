4fffda8a1024dc9711a42856641b9c35fc93754b
Shayegan Hooshyari <sh.hooshyari@gmail.com>
2020-12-09 11:11:59 +0330

    Fix redis connection

--- src/orders/orders.queue.ts
+++ src/orders/orders.queue.ts
@@ -2,13 +2,17 @@ import { Queue, QueueScheduler } from 'bullmq';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 // it is used for delayed jobs in ordersQueue
-const ordersQueueScheduler = new QueueScheduler('SendOrders');
+const ordersQueueScheduler = new QueueScheduler('SendOrders', {
+  connection: {
+    host: process.env.REDIS_QUEUE_URL,
+    port: 6379,
+  },
+});
 
 const ordersQueue = new Queue('SendOrders', {
   connection: {
-    host: process.env.REDIS_CACHE_URL,
+    host: process.env.REDIS_QUEUE_URL,
     port: 6379,
-    password: process.env.REDIS_CACHE_PASS,
   },
 });
 
--- src/orders/orders.worker.ts
+++ src/orders/orders.worker.ts
@@ -3,22 +3,31 @@ import { sendWithdrawTrade } from '../bot/bot';
 import logger from '../logger/winston';
 import { Worker } from 'bullmq';
 
-const sendProductProcessor = new Worker('SendOrders', async (job: { data: SendProductJob }) => {
-  sendWithdrawTrade(
-    job.data.toSteamId,
-    job.data.assetId,
-    job.data.appId,
-    job.data.contextId,
-    (err: Error, status: string, offerId: string) => {
-      if (err) {
-        logger.error(`sendDepositTradeError : ${err.message}`);
-        return;
-      }
-      logger.debug(
-        `send product to user ${job.data.toSteamId}, offer: ${offerId}, status: ${status}`,
-      );
+const sendProductProcessor = new Worker(
+  'SendOrders',
+  async (job: { data: SendProductJob }) => {
+    sendWithdrawTrade(
+      job.data.toSteamId,
+      job.data.assetId,
+      job.data.appId,
+      job.data.contextId,
+      (err: Error, status: string, offerId: string) => {
+        if (err) {
+          logger.error(`sendDepositTradeError : ${err.message}`);
+          return;
+        }
+        logger.debug(
+          `send product to user ${job.data.toSteamId}, offer: ${offerId}, status: ${status}`,
+        );
+      },
+    );
+  },
+  {
+    connection: {
+      host: process.env.REDIS_QUEUE_URL,
+      port: 6379,
     },
-  );
-});
+  },
+);
 
 export = sendProductProcessor;
